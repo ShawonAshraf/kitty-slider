2d5e48ddea14572c255935d2fc4b880ed214a9d7
ShawonAshraf <shawon13@live.com>
2018-10-08 14:52:45 +0600

    window resize

--- src/main/index.js
+++ src/main/index.js
@@ -18,9 +18,9 @@ function createWindow() {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    height: 970,
     useContentSize: true,
-    width: 1000,
+    width: 1150,
   });
 
   mainWindow.loadURL(winURL);
